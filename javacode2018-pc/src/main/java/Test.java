0cb450befd31416b02ae6c29f2e4d8dc4ea668a9
guanqing <guanqinghz@163.com>
2021-10-09 21:53:05 +0800

    Initial commit

---
+++ javacode2018-pc/src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @description: TODO 类描述
+ * @Author guanqing
+ * @Date 2021/10/7 19:07
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("maven-source-plugin test");
+    }
+}
